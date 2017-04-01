f64455cfe8b5cee9fc05c597df428bce74b12b9c
Andriy Orehov <andriyorehov@gmail.com>
2017-04-01 11:57:29 +0300

    use base template, use cdn for twemoji, fix css

--- test.py
+++ test.py
@@ -1,116 +1,124 @@
-from flask import Flask, render_template,request,redirect,url_for # For flask implementation
-from pymongo import MongoClient # Database connector
-from bson.objectid import ObjectId # For ObjectId to work
+from flask import Flask, render_template, request, redirect, url_for  # For flask implementation
+from pymongo import MongoClient  # Database connector
+from bson.objectid import ObjectId  # For ObjectId to work
 
-client = MongoClient('localhost', 27017)    #Configure the connection to the database
-db = client.camp2016    #Select the database
-todos = db.todo #Select the collection
+client = MongoClient('localhost', 27017)  # Configure the connection to the database
+db = client.camp2016  # Select the database
+todos = db.todo  # Select the collection
 
 app = Flask(__name__)
-title = "TODO with Flask"
-heading = "ToDo Reminder"
-#modify=ObjectId()
+
+# modify=ObjectId()
+
 
 def redirect_url():
     return request.args.get('next') or \
            request.referrer or \
            url_for('index')
 
+
 @app.route("/list")
-def lists ():
-	#Display the all Tasks
-	todos_l = todos.find()
-	a1="active"
-	return render_template('index.html',a1=a1,todos=todos_l,t=title,h=heading)
+def lists():
+    # Display the all Tasks
+    todos_l = todos.find()
+    a1 = "active"
+    return render_template('index.html', a1=a1, todos=todos_l)
+
 
 @app.route("/")
 @app.route("/uncompleted")
-def tasks ():
-	#Display the Uncompleted Tasks
-	todos_l = todos.find({"done":"no"})
-	a2="active"
-	return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading)
+def tasks():
+    # Display the Uncompleted Tasks
+    todos_l = todos.find({"done": "no"})
+    a2 = "active"
+    return render_template('index.html', a2=a2, todos=todos_l)
 
 
 @app.route("/completed")
-def completed ():
-	#Display the Completed Tasks
-	todos_l = todos.find({"done":"yes"})
-	a3="active"
-	return render_template('index.html',a3=a3,todos=todos_l,t=title,h=heading)
+def completed():
+    # Display the Completed Tasks
+    todos_l = todos.find({"done": "yes"})
+    a3 = "active"
+    return render_template('index.html', a3=a3, todos=todos_l)
+
 
 @app.route("/done")
-def done ():
-	#Done-or-not ICON
-	id=request.values.get("_id")
-	task=todos.find({"_id":ObjectId(id)})
-	if(task[0]["done"]=="yes"):
-		todos.update({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
-	else:
-		todos.update({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
-	redir=redirect_url()	# Re-directed URL i.e. PREVIOUS URL from where it came into this one
+def done():
+    # Done-or-not ICON
+    id = request.values.get("_id")
+    task = todos.find({"_id": ObjectId(id)})
+    if (task[0]["done"] == "yes"):
+        todos.update({"_id": ObjectId(id)}, {"$set": {"done": "no"}})
+    else:
+        todos.update({"_id": ObjectId(id)}, {"$set": {"done": "yes"}})
+    redir = redirect_url()  # Re-directed URL i.e. PREVIOUS URL from where it came into this one
 
-#	if(str(redir)=="http://localhost:5000/search"):
-#		redir+="?key="+id+"&refer="+refer
+    #	if(str(redir)=="http://localhost:5000/search"):
+    #		redir+="?key="+id+"&refer="+refer
 
-	return redirect(redir)
+    return redirect(redir)
 
-#@app.route("/add")
-#def add():
+
+# @app.route("/add")
+# def add():
 #	return render_template('add.html',h=heading,t=title)
 
 @app.route("/action", methods=['POST'])
-def action ():
-	#Adding a Task
-	name=request.values.get("name")
-	desc=request.values.get("desc")
-	date=request.values.get("date")
-	pr=request.values.get("pr")
-	todos.insert({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"no"})
-	return redirect("/list")
+def action():
+    # Adding a Task
+    name = request.values.get("name")
+    desc = request.values.get("desc")
+    date = request.values.get("date")
+    pr = request.values.get("pr")
+    todos.insert({"name": name, "desc": desc, "date": date, "pr": pr, "done": "no"})
+    return redirect("/list")
+
 
 @app.route("/remove")
-def remove ():
-	#Deleting a Task with various references
-	key=request.values.get("_id")
-	todos.remove({"_id":ObjectId(key)})
-	return redirect("/")
+def remove():
+    # Deleting a Task with various references
+    key = request.values.get("_id")
+    todos.remove({"_id": ObjectId(key)})
+    return redirect("/")
+
 
 @app.route("/update")
-def update ():
-	id=request.values.get("_id")
-	task=todos.find({"_id":ObjectId(id)})
-	return render_template('update.html',tasks=task,h=heading,t=title)
+def update():
+    id = request.values.get("_id")
+    task = todos.find({"_id": ObjectId(id)})
+    return render_template('update.html', tasks=task)
+
 
 @app.route("/action3", methods=['POST'])
-def action3 ():
-	#Updating a Task with various references
-	name=request.values.get("name")
-	desc=request.values.get("desc")
-	date=request.values.get("date")
-	pr=request.values.get("pr")
-	id=request.values.get("_id")
-	todos.update({"_id":ObjectId(id)}, {'$set':{ "name":name, "desc":desc, "date":date, "pr":pr }})
-	return redirect("/")
+def action3():
+    # Updating a Task with various references
+    name = request.values.get("name")
+    desc = request.values.get("desc")
+    date = request.values.get("date")
+    pr = request.values.get("pr")
+    id = request.values.get("_id")
+    todos.update({"_id": ObjectId(id)}, {'$set': {"name": name, "desc": desc, "date": date, "pr": pr}})
+    return redirect("/")
+
 
 @app.route("/search", methods=['GET'])
 def search():
-	#Searching a Task with various references
+    # Searching a Task with various references
 
-	key=request.values.get("key")
-	refer=request.values.get("refer")
-	if(key=="_id"):
-		todos_l = todos.find({refer:ObjectId(key)})
-	else:
-		todos_l = todos.find({refer:key})
-	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
+    key = request.values.get("key")
+    refer = request.values.get("refer")
+    if (key == "_id"):
+        todos_l = todos.find({refer: ObjectId(key)})
+    else:
+        todos_l = todos.find({refer: key})
+    return render_template('searchlist.html', todos=todos_l)
+
 
 @app.route("/about")
 def about():
-	return render_template('credits.html',t=title,h=heading)
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
 # Careful with the debug mode..
-
-
