f32b1c066c07405db2500b0033a23c9a358ed8a7
Andriy Orehov <andriyorehov@gmail.com>
2017-04-03 21:19:46 +0300

    use flask_mongoengine and flask_wtf, refactor code add data picker

--- test.py
+++ test.py
@@ -1,117 +1,103 @@
-from flask import Flask, render_template, request, redirect, url_for  # For flask implementation
-from pymongo import MongoClient  # Database connector
-from bson.objectid import ObjectId  # For ObjectId to work
-
-client = MongoClient('localhost', 27017)  # Configure the connection to the database
-db = client.camp2016  # Select the database
-todos = db.todo  # Select the collection
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mongoengine import MongoEngine
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, SelectField, SubmitField
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'project1',
+}
+app.config['SECRET_KEY'] = 'hard to guess string'
+db = MongoEngine(app)
 
-# modify=ObjectId()
+
+class UserForm(FlaskForm):
+    name = StringField('Task Name')
+    desc = StringField('Description Name')
+    date = DateField('Date', format="%m/%d/%Y")
+    pr = SelectField(u'Priority', choices=[('1', '1'), ('2', '2'), ('3', '3')])
+    submit = SubmitField('Отправить')
+
+
+class User(db.Document):
+    name = db.StringField()
+    desc = db.StringField()
+    date = db.DateTimeField()
+    pr = db.StringField()
+    done = db.StringField(default='no')
 
 
 def redirect_url():
-    return request.args.get('next') or \
-           request.referrer or \
-           url_for('index')
+    return request.args.get('next') or request.referrer or url_for('index')
 
 
-@app.route("/list")
+@app.route("/", methods=('GET', 'POST'))
 def lists():
-    # Display the all Tasks
-    todos_l = todos.find()
+    form = UserForm(request.form)
+    todos_l = User.objects
     a1 = "active"
-    return render_template('index.html', a1=a1, todos=todos_l)
+    if request.method == 'POST':
+        User(form.name.data, form.desc.data, form.date.data,
+             form.pr.data).save()
+    return render_template('index.html', a1=a1, todos=todos_l, form=form)
 
 
-@app.route("/")
 @app.route("/uncompleted")
 def tasks():
     # Display the Uncompleted Tasks
-    todos_l = todos.find({"done": "no"})
+    form = UserForm(request.form)
+    todos_l = User.objects(done="no")
     a2 = "active"
-    return render_template('index.html', a2=a2, todos=todos_l)
+    return render_template('index.html', a2=a2, todos=todos_l, form=form)
 
 
 @app.route("/completed")
 def completed():
     # Display the Completed Tasks
-    todos_l = todos.find({"done": "yes"})
+    form = UserForm(request.form)
+    todos_l = User.objects(done="yes")
     a3 = "active"
-    return render_template('index.html', a3=a3, todos=todos_l)
+    return render_template('index.html', a3=a3, todos=todos_l, form=form)
 
 
 @app.route("/done")
 def done():
     # Done-or-not ICON
-    id = request.values.get("_id")
-    task = todos.find({"_id": ObjectId(id)})
-    if (task[0]["done"] == "yes"):
-        todos.update({"_id": ObjectId(id)}, {"$set": {"done": "no"}})
+    _id = request.values.get("_id")
+    rem = User.objects.get(id=_id)
+    if rem.done == 'yes':
+        User.objects(id=_id).update(done='no')
     else:
-        todos.update({"_id": ObjectId(id)}, {"$set": {"done": "yes"}})
-    redir = redirect_url()  # Re-directed URL i.e. PREVIOUS URL from where it came into this one
-
-    #	if(str(redir)=="http://localhost:5000/search"):
-    #		redir+="?key="+id+"&refer="+refer
-
-    return redirect(redir)
-
-
-# @app.route("/add")
-# def add():
-#	return render_template('add.html',h=heading,t=title)
-
-@app.route("/action", methods=['POST'])
-def action():
-    # Adding a Task
-    name = request.values.get("name")
-    desc = request.values.get("desc")
-    date = request.values.get("date")
-    pr = request.values.get("pr")
-    todos.insert({"name": name, "desc": desc, "date": date, "pr": pr, "done": "no"})
-    return redirect("/list")
+        User.objects(id=_id).update(done='yes')
+    return redirect(url_for('lists'))
 
 
 @app.route("/remove")
 def remove():
     # Deleting a Task with various references
-    key = request.values.get("_id")
-    todos.remove({"_id": ObjectId(key)})
+    _id = request.values.get("_id")
+    rem = User.objects.get(id=_id)
+    rem.delete()
     return redirect("/")
 
 
-@app.route("/update")
+@app.route("/update", methods=('GET', 'POST'))
 def update():
-    id = request.values.get("_id")
-    task = todos.find({"_id": ObjectId(id)})
-    return render_template('update.html', tasks=task)
-
-
-@app.route("/action3", methods=['POST'])
-def action3():
-    # Updating a Task with various references
-    name = request.values.get("name")
-    desc = request.values.get("desc")
-    date = request.values.get("date")
-    pr = request.values.get("pr")
-    id = request.values.get("_id")
-    todos.update({"_id": ObjectId(id)}, {'$set': {"name": name, "desc": desc, "date": date, "pr": pr}})
-    return redirect("/")
+    _id = request.values.get("_id")
+    task = User.objects.get(id=_id)
+    form = UserForm(request.form)
+    if request.method == 'POST':
+        _id = request.values.get("_id")
+        print(_id)
+        User.objects(id=_id).update(name=form.name.data, desc=form.desc.data, date=form.date.data, pr=form.pr.data)
+        return redirect(url_for('lists'))
+    return render_template('update.html', task=task, form=form)
 
 
 @app.route("/search", methods=['GET'])
 def search():
-    # Searching a Task with various references
-
-    key = request.values.get("key")
-    refer = request.values.get("refer")
-    if (key == "_id"):
-        todos_l = todos.find({refer: ObjectId(key)})
-    else:
-        todos_l = todos.find({refer: key})
-    return render_template('searchlist.html', todos=todos_l)
+    # todo search
+    pass
 
 
 @app.route("/about")
@@ -121,4 +107,3 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
-# Careful with the debug mode..
